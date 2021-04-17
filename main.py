bc8a777e00b63c27a22c71961822194626a8b7ba
inpruner <53217289+inpruner@users.noreply.github.com>
2021-04-17 23:23:29 +0300

    Implementing classes 'Unit' and 'Stream' and also classes for parcing DB

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from sqlalchemy import Column
+from sqlalchemy import Integer, Text
+from sqlalchemy.orm import declarative_base
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Unit:
+    all_units = {}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self, name=None, load_max=None):
+        self.name = name
+        self.__load_max = load_max
+        self.streams_in = dict()
+        self.streams_out = dict()
+        Unit.all_units[self.name] = self
+
+    def __repr__(self):
+        return f"<Unit(name={self.name}," \
+               f" __load_max={self.__load_max}," \
+               f" streams_in={self.streams_in}," \
+               f" streams_out={self.streams_out})>"
+
+
+class AVTUnit(Unit):
+    pass  # types?
+
+
+class RerunningUnit(Unit):
+    pass
+
+
+class Stream:
+    all_streams = {}
+
+    def __init__(self, name=None):
+        self.name = name
+        self.dep_units = []
+        self.dst_units = []
+        Stream.all_streams[self.name] = self
+
+    def __repr__(self):
+        return f"<Stream(name={self.name}," \
+               f" dep_units={self.dep_units}," \
+               f" dst_units={self.dst_units})>"
+
+
+__Base = declarative_base()
+
+
+class __UnitTable(__Base):
+    __tablename__ = 'unit'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Text)
+    type = Column(Integer)
+
+    def __repr__(self):
+        return f"<Unit(id={self.id}, name={self.name}, type={self.type})>"
+
+
+class __StreamTable(__Base):
+    __tablename__ = 'stream'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Text)
+
+    def __repr__(self):
+        return f"<Stream(id={self.id}, name={self.name})>"
+
+
+class __UnitMaterialTable(__Base):
+    __tablename__ = 'unit_material'
+    unit_id = Column(Integer, primary_key=True)
+    stream_id = Column(Integer, primary_key=True)
+    feed_flag = Column(Integer)
+
+    def __repr__(self):
+        return f"<UnitMaterial(unit_id={self.unit_id}," \
+               f" stream_id={self.stream_id}, feed_flag={self.feed_flag})>"
+
+
+class __LoadMaxTable(__Base):
+    __tablename__ = 'load_max'
+    unit_id = Column(Integer, primary_key=True)
+    value = Column(Integer)
+
+    def __repr__(self):
+        return f"<LoadMax(unit_id={self.unit_id}, value={self.value})>"
